0e778dec817d6a78e904b11fa905d3180b8d17c1
Jai Kathuria <jaikathuria@live.com>
2017-02-02 22:12:07 +0530

    Mod Division

--- 16-String-Split-and-Join.py
+++ 16-String-Split-and-Join.py
@@ -1,3 +1,3 @@
 def split_and_join(line):
     # writ your code here
-    return "-".join(line.split(" "))
+    return "-".join(line.split(" "))t
---
+++ 32-Mod-Division.py
@@ -0,0 +1,8 @@
+from __future__ import division
+a = int(raw_input())
+b = int(raw_input())
+div = a//b
+rem = a%b
+print div
+print rem
+print "(%s, %s)" % (div,rem)
